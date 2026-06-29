4c46232b86fd9df88a24c3247b90db190b55a0ce
Shivam Kumar <shivamkumar@Shivams-MacBook-Air-2.local>
2026-06-29 22:29:18 +0530

    Updated project

---
+++ Day 2/day 3/increament_decreament.java
@@ -0,0 +1,9 @@
+public class increament_decreament {
+    public static void main(String[] args) {
+        int a=1,b=2;
+        int c=a++ + ++a + --b + b++;
+        System.out.println(c);
+        
+    }
+    
+}
