2b7bc4b74d6b70d406a291fc080c6680c35a32a9
Shivam Kumar <kumarys8922@gmail.com>
2026-07-09 17:08:23 +0530

    sec_large

---
+++ Day 3/sec_large.java
@@ -0,0 +1,12 @@
+public class sec_large {
+    public static void main(String[] args) {
+        int arr[]={3,4,5,8,9};
+        int r=0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>r) ;
+                //r=arr[i];
+                
+        }
+
+    }
+}
