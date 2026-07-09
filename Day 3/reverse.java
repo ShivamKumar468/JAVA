6698092a9181e421d12c312d79791736aba39e37
Shivam Kumar <kumarys8922@gmail.com>
2026-07-09 17:07:17 +0530

    reverse

---
+++ Day 3/reverse.java
@@ -0,0 +1,11 @@
+public class reverse {
+    public static void main(String[] args) {
+        int arr[]= {2,4,5,2,5};
+        
+        for(int i=arr.length-1; i>=0; i--){
+            System.out.print(arr[i]+",");
+
+        }
+        
+    }
+}
