427a1600ae34d09687ffc09132cd91f9512c90f3
Shivam Kumar <kumarys8922@gmail.com>
2026-07-10 22:34:10 +0530

    sum_e

---
+++ Day 3/sum_e.java
@@ -0,0 +1,12 @@
+public class sum_e {
+    public static void main(String[] args) {
+        int s[] ={3,4,5,6,1};
+
+        int sum=0;
+        for(int i=0; i<s.length; i++){
+            sum+=s[i];
+        }
+        System.out.println(sum);;
+    }
+    
+}
