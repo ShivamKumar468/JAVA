901800c229769bd80c4a6357b38b1cfdb76b73ce
Shivam Kumar <kumarys8922@gmail.com>
2026-07-11 23:17:42 +0530

    function

---
+++ Day 3/day 4/function.java
@@ -0,0 +1,10 @@
+public class function {
+    public static void solve() {
+        int a=10, b=20;
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        solve();
+    }
+
+}
