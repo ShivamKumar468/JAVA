e288dac7f4a6b17c31258e88ec85b26def349314
Shivam Kumar <kumarys8922@gmail.com>
2026-07-12 22:15:00 +0530

    static

---
+++ Day 4/sta.java
@@ -0,0 +1,14 @@
+public class sta{
+    public static int display(){
+        int a= 10, b=20;
+        int sum = a+b;
+        return sum;
+    }
+    public static void main(String[] args){
+        System.out.println(display());
+        sta obj = new sta();
+        obj.display();
+
+    }
+
+}
