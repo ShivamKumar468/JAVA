90a9f6df32cfae5dbf127f7e90ceb989340917f8
Shivam Kumar <kumarys8922@gmail.com>
2026-07-12 22:14:25 +0530

    function return

---
+++ Day 3/day 4/function_return.java
@@ -0,0 +1,10 @@
+public class function_return {
+    public static int solve(){
+        int a=10, b= 20;
+        int sum= a+b;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
