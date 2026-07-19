daf4faa114a44c55e015afd68608f25bd635072c
Shivam Kumar <kumarys8922@gmail.com>
2026-07-19 21:55:47 +0530

    abstraction

---
+++ Abstraction.java
@@ -0,0 +1,18 @@
+abstract class Animal{
+    abstract void sound();
+}
+
+class Dog extends Animal{
+    void sound(){
+        System.out.println("Barks");
+    }
+}
+
+
+public class Abstraction {
+    public static void main(String[] args) {
+        Dog d= new Dog();
+        d.sound();
+        
+    }
+}
